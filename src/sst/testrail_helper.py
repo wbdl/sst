aea07863f9485b91d0ab34f55f5fb7b6fee4c12f
Bassoon08 <mshafer@dramafever.com>
2016-07-26 15:33:49 -0400

    update testrail_helper module to additionally post an individual test case result, and create APITestStatus class for easier use of test case status IDs

--- src/sst/testrail_helper.py
+++ src/sst/testrail_helper.py
@@ -19,3 +19,20 @@ def send_results(run_id=88):
         )
     except Exception as e:
         logger.debug("Could not send TestRail results \n" + str(e))
+
+# add individual test case result to test run
+def send_result(case_id, status_id, comment=None, run_id=88):
+    try:
+        run = client.send_post('add_result_for_case/{}/{}'.format(run_id, case_id),
+            {
+            	"status_id": status_id,
+            	"comment": comment
+             }
+        )
+    except Exception as e:
+        logger.debug("Could not send TestRail results \n" + str(e))
+
+
+class APITestStatus(object):
+
+    PASSED, BLOCKED, RETEST, RETEST, FAILED = range(1, 6)
