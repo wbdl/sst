5fe65af097822bccec0af992b66bbf4380efad66
Bassoon08 <mshafer@dramafever.com>
2016-08-01 16:28:46 -0400

    store the JSON test results as a text file before posting to the API, for either individual test cases within the corresponding test directory, or one result file for the full suite in the parent directory

--- src/sst/testrail_helper.py
+++ src/sst/testrail_helper.py
@@ -30,6 +30,7 @@ def create_test_run(case_ids):
 
 # add test case results to test run
 def send_results():
+    store_json_results(run_results)
     try:
         run = client.send_post('add_results_for_cases/{}'.format(run_id),
     		{ "results": run_results }
@@ -39,15 +40,22 @@ def send_results():
 
 # add individual test case result to test run
 def send_result(case_id, status_id, comment=None):
+    result = {
+        "status_id": status_id,
+        "comment": comment
+    }
+    store_json_results(result, case_id)
     try:
-        run = client.send_post('add_result_for_case/{}/{}'.format(run_id, case_id),
-            {
-            	"status_id": status_id,
-            	"comment": comment
-             }
-        )
+        run = client.send_post('add_result_for_case/{}/{}'
+                               .format(run_id, case_id), result)
     except Exception as e:
         logger.debug("Could not send TestRail results \n" + str(e))
+
+def store_json_results(result, case_id=None):
+    path = '../' if not case_id else ''
+    case_id = case_id or 'suite'
+    with open('{}json_results_{}.txt'.format(path, case_id), 'w') as outfile:
+        json.dump(result, outfile)
 
 
 class APITestStatus(object):
