99745027710399b30e49c736fd5f31f02fa5e323
Bassoon08 <mshafer@dramafever.com>
2017-02-09 16:49:42 -0500

    add appium_config to set_client_credentials to pull in capabilities from the client project

--- src/sst/runtests.py
+++ src/sst/runtests.py
@@ -159,6 +159,9 @@ def set_client_credentials(client):
     elif client == 'saucelabs':
         return find_client_credentials('sauce_config')
 
+    elif client == 'appium':
+        return find_client_credentials('appium_config')
+
 def find_shared_directory(test_dir, shared_directory):
     """This function is responsible for finding the shared directory.
     It implements the following rule:
