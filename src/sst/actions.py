08311285d87a243acd7bb8fdfece3f91ee4b079f
rmdaggett <rmdaggett@dramafever.com>
2017-04-20 09:59:39 -0400

    use poll_for_element in _waitforbody instead of wait_for which heavily uses timeouts.

--- src/sst/actions.py
+++ src/sst/actions.py
@@ -52,6 +52,7 @@ from pdb import set_trace as debug
 from urlparse import urljoin, urlparse
 
 from selenium.webdriver.common import action_chains, keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -67,7 +68,7 @@ from selenium.common.exceptions import (
 
 from sst import config
 
-__all__ = [
+__all__ = (
     'accept_alert', 'add_cleanup', 'assert_attribute', 'assert_button',
     'assert_checkbox', 'assert_checkbox_value', 'assert_css_property',
     'assert_displayed', 'assert_dropdown', 'assert_dropdown_value',
@@ -94,7 +95,7 @@ __all__ = [
     'switch_to_active_window', 'switch_to_frame', 'switch_to_window',
     'take_screenshot', 'toggle_checkbox', 'wait_for', 'wait_for_and_refresh',
     'write_textfield'
-]
+)
 
 
 _check_flags = True
@@ -1373,8 +1374,7 @@ def get_element_by_xpath(selector):
 
 
 def _waitforbody():
-    wait_for(get_element, tag='body')
-
+    poll_for_element((By.TAG_NAME, 'body'))
 
 def get_page_source():
     """Gets the source of the current page."""
@@ -1881,6 +1881,7 @@ def poll_for_element(locator, wait=10, frequency=1):
 
     """
     try:
+        logger.debug("Waiting for element: {}".format(locator))
         return (WebDriverWait(_test.browser, wait, poll_frequency=frequency)
                .until(EC.visibility_of_element_located(locator)))
     except TimeoutException:
