e98e3e9d0b53d506202436701c8b8ae6f8dbde8f
Bassoon08 <mshafer@dramafever.com>
2020-07-11 20:27:23 -0400

    replace with gurock supplied binding for python 3

--- src/sst/testrail_api/testrail.py
+++ src/sst/testrail_api/testrail.py
@@ -1,88 +1,102 @@
-#
-# TestRail API binding for Python 2.x (API v2, available since 
-# TestRail 3.0)
-#
-# Learn more:
-#
-# http://docs.gurock.com/testrail-api2/start
-# http://docs.gurock.com/testrail-api2/accessing
-#
-# Copyright Gurock Software GmbH. See license.md for details.
-#
+"""TestRail API binding for Python 3.x.
 
-from future import standard_library
-standard_library.install_aliases()
-from builtins import object
-import urllib.request, urllib.error, urllib.parse, json, base64
+(API v2, available since TestRail 3.0)
 
-class APIClient(object):
-	def __init__(self, base_url):
-		self.user = ''
-		self.password = ''
-		if not base_url.endswith('/'):
-			base_url += '/'
-		self.__url = base_url + 'index.php?/api/v2/'
+Compatible with TestRail 3.0 and later.
 
-	#
-	# Send Get
-	#
-	# Issues a GET request (read) against the API and returns the result
-	# (as Python dict).
-	#
-	# Arguments:
-	#
-	# uri                 The API method to call including parameters
-	#                     (e.g. get_case/1)
-	#
-	def send_get(self, uri):
-		return self.__send_request('GET', uri, None)
+Learn more:
 
-	#
-	# Send POST
-	#
-	# Issues a POST request (write) against the API and returns the result
-	# (as Python dict).
-	#
-	# Arguments:
-	#
-	# uri                 The API method to call including parameters
-	#                     (e.g. add_case/1)
-	# data                The data to submit as part of the request (as
-	#                     Python dict, strings must be UTF-8 encoded)
-	#
-	def send_post(self, uri, data):
-		return self.__send_request('POST', uri, data)
+http://docs.gurock.com/testrail-api2/start
+http://docs.gurock.com/testrail-api2/accessing
 
-	def __send_request(self, method, uri, data):
-		url = self.__url + uri
-		request = urllib.request.Request(url)
-		if (method == 'POST'):
-			request.data = json.dumps(data)
-		auth = base64.b64encode('{}:{}'.format(
-			self.user, self.password).encode('ascii'))
-		request.add_header('Authorization', 'Basic %s' % auth)
-		request.add_header('Content-Type', 'application/json')
+Copyright Gurock Software GmbH. See license.md for details.
+"""
 
-		e = None
-		try:
-			response = urllib.request.urlopen(request).read()
-		except urllib.error.HTTPError as e:
-			response = e.read()
+import base64
+import json
 
-		if response:
-			result = json.loads(response)
-		else:
-			result = {}
+import requests
 
-		if e != None:
-			if result and 'error' in result:
-				error = '"' + result['error'] + '"'
-			else:
-				error = 'No additional error message received'
-			raise APIError('TestRail API returned HTTP %s (%s)' % 
-				(e.code, error))
 
-		return result
+class APIClient:
+    def __init__(self, base_url):
+        self.user = ''
+        self.password = ''
+        if not base_url.endswith('/'):
+            base_url += '/'
+        self.__url = base_url + 'index.php?/api/v2/'
+
+    def send_get(self, uri, filepath=None):
+        """Issue a GET request (read) against the API.
+
+        Args:
+            uri: The API method to call including parameters, e.g. get_case/1.
+            filepath: The path and file name for attachment download; used only
+                for 'get_attachment/:attachment_id'.
+
+        Returns:
+            A dict containing the result of the request.
+        """
+        return self.__send_request('GET', uri, filepath)
+
+    def send_post(self, uri, data):
+        """Issue a POST request (write) against the API.
+
+        Args:
+            uri: The API method to call, including parameters, e.g. add_case/1.
+            data: The data to submit as part of the request as a dict; strings
+                must be UTF-8 encoded. If adding an attachment, must be the
+                path to the file.
+
+        Returns:
+            A dict containing the result of the request.
+        """
+        return self.__send_request('POST', uri, data)
+
+    def __send_request(self, method, uri, data):
+        url = self.__url + uri
+
+        auth = str(
+            base64.b64encode(
+                bytes('%s:%s' % (self.user, self.password), 'utf-8')
+            ),
+            'ascii'
+        ).strip()
+        headers = {'Authorization': 'Basic ' + auth}
+
+        if method == 'POST':
+            if uri[:14] == 'add_attachment':    # add_attachment API method
+                files = {'attachment': (open(data, 'rb'))}
+                response = requests.post(url, headers=headers, files=files)
+                files['attachment'].close()
+            else:
+                headers['Content-Type'] = 'application/json'
+                payload = bytes(json.dumps(data), 'utf-8')
+                response = requests.post(url, headers=headers, data=payload)
+        else:
+            headers['Content-Type'] = 'application/json'
+            response = requests.get(url, headers=headers)
+
+        if response.status_code > 201:
+            try:
+                error = response.json()
+            except:     # response.content not formatted as JSON
+                error = str(response.content)
+            raise APIError('TestRail API returned HTTP %s (%s)' %
+                           (response.status_code, error))
+        else:
+            if uri[:15] == 'get_attachment/':   # Expecting file, not JSON
+                try:
+                    open(data, 'wb').write(response.content)
+                    return (data)
+                except:
+                    return ("Error saving attachment.")
+            else:
+                try:
+                    return response.json()
+                except:  # Nothing to return
+                    return {}
+
 
 class APIError(Exception):
-	pass
+    pass
